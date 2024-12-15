42bfe3cfa0298215fc03c15f61dfc288c1b7e248
dktr0348 <dktr0348@gmail.com>
2024-12-15 13:02:25 +0300

    c декоратором

--- app/database/requests.py
+++ app/database/requests.py
@@ -1,16 +1,24 @@
 from app.database.models import async_session
 from app.database.models import Category, User, Item, Basket
 from sqlalchemy import select, update, delete
+from functools import wraps
 
-async def get_categories():
-    async with async_session() as session:
-        return await session.scalars(select(Category))
+def connection(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        async with async_session() as session:
+            return await func(session, *args, **kwargs)
+    return wrapper
+
+@connection
+async def get_categories(session):
+    return await session.scalars(select(Category))
    
-async def get_items_by_category(category_id):
-    async with async_session() as session:
-        return await session.scalars(select(Item).where(Item.category_id==category_id))
+@connection
+async def get_items_by_category(session, category_id):
+    return await session.scalars(select(Item).where(Item.category_id==category_id))
    
-async def get_item(item_id):
-    async with async_session() as session:
-        return await session.scalar(select(Item).where(Item.id==item_id))
+@connection
+async def get_item(session, item_id):
+    return await session.scalar(select(Item).where(Item.id==item_id))
    
